6efb30bcf2e313519ab03b8c4f053b8e2e957fae
Gourab Singha <gaurabsingha16@gmail.com>
2024-09-16 21:36:23 +0530

    Update Resume

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -48,7 +48,7 @@ export const Banner = () => {
   }
 
   const checkResume = () => {
-    window.open('https://drive.google.com/file/d/1cOhLryhdca0Tt-9iE1Ymzva6Hb24ggK-/view?usp=drive_link', '_blank');
+    window.open('https://drive.google.com/file/d/1uuZcvHWczYI_NKiw9lVNnVJQALvKsOBM/view?usp=drive_link', '_blank');
   };
 
   return (
